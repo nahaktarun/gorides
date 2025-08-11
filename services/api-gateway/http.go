06b26cdc2ade6782bb18c1174beea8503a8f6b3b
nahaktarun <tarunnahak25@gmail.com>
2025-08-11 18:31:57 +0530

    Added the get routes functionality

--- services/api-gateway/http.go
+++ services/api-gateway/http.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"ride-sharing/shared/contracts"
 )
@@ -13,7 +16,7 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to parse JSON data", http.StatusBadRequest)
 		return
 	}
-	// TODO: call trip service
+
 	defer r.Body.Close()
 
 	// validation
@@ -21,8 +24,24 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "user id is required", http.StatusBadRequest)
 		return
 	}
+	jsonBody, _ := json.Marshal(reqBody)
+	reader := bytes.NewReader(jsonBody)
 
-	response := contracts.APIResponse{Data: "ok"}
+	resp, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	log.Println(resp)
+	fmt.Println("here...")
+	defer resp.Body.Close()
+
+	var respBody any
+	if err := json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
+		http.Error(w, "failed to parse response", http.StatusBadRequest)
+		return
+	}
+	response := contracts.APIResponse{Data: respBody}
 
 	writeJSON(w, http.StatusCreated, response)
 }
--- services/api-gateway/types.go
+++ services/api-gateway/types.go
@@ -3,7 +3,7 @@ package main
 import "ride-sharing/shared/types"
 
 type previewTripRequest struct {
-	UserID      string
-	Pickup      types.Coordinate
-	Destination types.Coordinate
+	UserID      string           `json:"userID"`
+	Pickup      types.Coordinate `json:"pickup"`
+	Destination types.Coordinate `json:"destination"`
 }
